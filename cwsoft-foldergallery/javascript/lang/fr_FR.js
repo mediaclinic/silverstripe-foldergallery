e6a925cc0d9ea1129fa3180cd96bc2f10d5a830d
Juan Molina <juan.molina@club.fr>
2012-11-02 22:33:40 +0100

    Updated French translation

--- cwsoft-foldergallery/javascript/lang/fr_FR.js
+++ cwsoft-foldergallery/javascript/lang/fr_FR.js
@@ -1,5 +1,5 @@
 ss.i18n.addDictionary('fr_FR', {
 	'cwsoft-foldergallery.FULL_SCALE': "Taille réelle",
-	'cwsoft-foldergallery.START_SLIDESHOW': "start slideshow",
-	'cwsoft-foldergallery.STOP_SLIDESHOW': "stop slideshow"
+	'cwsoft-foldergallery.START_SLIDESHOW': "démarrer diaporama",
+	'cwsoft-foldergallery.STOP_SLIDESHOW': "arrêter diaporama"
 });
