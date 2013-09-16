1e497f5688ae4cc6aed46c746cfec4e1a10232c1
cwsoft <info@cwsoft.de>
2013-09-16 10:47:16 +0200

    MINOR: correcting file comment header

--- cwsoft-foldergallery/javascript/cwsoft-foldergallery.js
+++ cwsoft-foldergallery/javascript/cwsoft-foldergallery.js
@@ -5,7 +5,7 @@
  * 
  * LICENSE: GNU General Public License 3.0
  * 
- * @platform    CMS SilverStripe 3.1.x (or higher)
+ * @platform    CMS SilverStripe 3.0.x
  * @package     cwsoft-foldergallery
  * @author      cwsoft (http://cwsoft.de)
  * @copyright   cwsoft
